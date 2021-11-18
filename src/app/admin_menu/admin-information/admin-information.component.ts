92d3520cbfa3b3a09c50a3db08b9070686959186
hgh0328 <56950366+hgh0328@users.noreply.github.com>
2021-11-18 14:06:59 +0900

    UI 모든 페이지 제작 완료(21.11.18) 이후 오류 수정 작업 예정

--- src/app/admin_menu/admin-information/admin-information.component.ts
+++ src/app/admin_menu/admin-information/admin-information.component.ts
@@ -11,41 +11,41 @@ export interface PeriodicElement {
 	codename : string;
 	price : string;
 	purchase : string;
-	profits : string;
+	profits : number;
 	grouping : string;
 	selling : string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
 
-  {code: "290520", codename: '신도기연', price: "16,000원", purchase: '17,000원', profits: '- 10.60%', grouping:'관리자, 특별회원', selling: '0'},
-  {code: "093320", codename: '케이아이엔엑스', price: "44,000원", purchase: '72,000원', profits: '- 38.60%', grouping:'전체', selling: '0'},
-  {code: "102710", codename: '이엔에프테크놀로지', price: "29,000원", purchase: '44,000원', profits: '- 34.60%', grouping:'일반회원', selling: '0'},
-  {code: "192440", codename: '슈피겐코리아', price: "48,000원", purchase: '62,500원', profits: '- 22.60%', grouping:'특별회원', selling: '0'},
-  {code: "187870", codename: '디바이스이엔지', price: "30,400원", purchase: '38,100원', profits: '- 20.20%', grouping:'특별회원', selling: '0'},
-  {code: "294090", codename: '이오플로우', price: "55,800원", purchase: '55,100원', profits: '+ 1.5%', grouping:'일반회원', selling: '0'},
-  {code: "200130", codename: '콜마비앤에이치', price: "35,800원", purchase: '49,100원', profits: '- 29.10%', grouping:'특별회원', selling: '0'},
-  {code: "353200", codename: '대덕전자', price: "17,050원", purchase: '18,200원', profits: '- 6.30%', grouping:'전체', selling: '0'},
-  {code: "030210", codename: 'KTB투자증권', price: "6,500원", purchase: '72,000원', profits: '- 9.70%', grouping:'관리자', selling: '0'},
-  {code: "003530", codename: '한화투자증권', price: "5,800원", purchase: '4,780원', profits: '+ 23.00%', grouping:'전체', selling: '0'},
-  {code: "001450", codename: '현대해상', price: "28,800원", purchase: '25,200원', profits: '+ 11.30%', grouping:'전체', selling: '0'},
-  {code: "066970", codename: '엘엔에프', price: "214,400원", purchase: '185,500원', profits: '+ 16.60%', grouping:'일반회원', selling: '0'},
-  {code: "032640", codename: 'LG유플러스', price: "14,800원", purchase: '14,300원', profits: '+ 3.50%', grouping:'특별회원', selling: '0'},
-  {code: "096240", codename: '청담러닝', price: "31,800원", purchase: '27,200원', profits: '+ 14.40%', grouping:'일반회원', selling: '0'},
-  {code: "290524", codename: '신도기연', price: "16,000원", purchase: '17,000원', profits: '- 10.60%', grouping:'관리자, 특별회원', selling: '0'},
-  {code: "093321", codename: '케이아이엔엑스', price: "44,000원", purchase: '72,000원', profits: '- 38.60%', grouping:'전체', selling: '0'},
-  {code: "102711", codename: '이엔에프테크놀로지', price: "29,000원", purchase: '44,000원', profits: '- 34.60%', grouping:'일반회원', selling: '0'},
-  {code: "192442", codename: '슈피겐코리아', price: "48,000원", purchase: '62,500원', profits: '- 22.60%', grouping:'특별회원', selling: '0'},
-  {code: "187873", codename: '디바이스이엔지', price: "30,400원", purchase: '38,100원', profits: '- 20.20%', grouping:'특별회원', selling: '0'},
-  {code: "294094", codename: '이오플로우', price: "55,800원", purchase: '55,100원', profits: '+ 1.5%', grouping:'일반회원', selling: '0'},
-  {code: "200135", codename: '콜마비앤에이치', price: "35,800원", purchase: '49,100원', profits: '- 29.10%', grouping:'특별회원', selling: '0'},
-  {code: "353201", codename: '대덕전자', price: "17,050원", purchase: '18,200원', profits: '- 6.30%', grouping:'전체', selling: '0'},
-  {code: "030212", codename: 'KTB투자증권', price: "6,500원", purchase: '72,000원', profits: '- 9.70%', grouping:'관리자', selling: '0'},
-  {code: "003533", codename: '한화투자증권', price: "5,800원", purchase: '4,780원', profits: '+ 23.00%', grouping:'전체', selling: '0'},
-  {code: "001454", codename: '현대해상', price: "28,800원", purchase: '25,200원', profits: '+ 11.30%', grouping:'전체', selling: '0'},
-  {code: "066975", codename: '엘엔에프', price: "214,400원", purchase: '185,500원', profits: '+ 16.60%', grouping:'일반회원', selling: '0'},
-  {code: "032646", codename: 'LG유플러스', price: "14,800원", purchase: '14,300원', profits: '+ 3.50%', grouping:'특별회원', selling: '0'},
-  {code: "096247", codename: '청담러닝', price: "31,800원", purchase: '27,200원', profits: '+ 14.40%', grouping:'일반회원', selling: '0'},
+  {code: "290520", codename: '신도기연', price: "16,000원", purchase: '17,000원', profits: - 10.60, grouping:'관리자, 특별회원', selling: '0'},
+  {code: "093320", codename: '케이아이엔엑스', price: "44,000원", purchase: '72,000원', profits: - 38.60, grouping:'전체', selling: '0'},
+  {code: "102710", codename: '이엔에프테크놀로지', price: "29,000원", purchase: '44,000원', profits: - 34.60, grouping:'일반회원', selling: '0'},
+  {code: "192440", codename: '슈피겐코리아', price: "48,000원", purchase: '62,500원', profits: - 22.60, grouping:'특별회원', selling: '0'},
+  {code: "187870", codename: '디바이스이엔지', price: "30,400원", purchase: '38,100원', profits: - 20.20, grouping:'특별회원', selling: '0'},
+  {code: "294090", codename: '이오플로우', price: "55,800원", purchase: '55,100원', profits: + 1.5, grouping:'일반회원', selling: '0'},
+  {code: "200130", codename: '콜마비앤에이치', price: "35,800원", purchase: '49,100원', profits: - 29.10, grouping:'특별회원', selling: '0'},
+  {code: "353200", codename: '대덕전자', price: "17,050원", purchase: '18,200원', profits: - 6.30, grouping:'전체', selling: '0'},
+  {code: "030210", codename: 'KTB투자증권', price: "6,500원", purchase: '72,000원', profits: - 9.70, grouping:'관리자', selling: '0'},
+  {code: "003530", codename: '한화투자증권', price: "5,800원", purchase: '4,780원', profits: + 23.00, grouping:'전체', selling: '0'},
+  {code: "001450", codename: '현대해상', price: "28,800원", purchase: '25,200원', profits: + 11.30, grouping:'전체', selling: '0'},
+  {code: "066970", codename: '엘엔에프', price: "214,400원", purchase: '185,500원', profits: + 16.60, grouping:'일반회원', selling: '0'},
+  {code: "032640", codename: 'LG유플러스', price: "14,800원", purchase: '14,300원', profits: + 3.50, grouping:'특별회원', selling: '0'},
+  {code: "096240", codename: '청담러닝', price: "31,800원", purchase: '27,200원', profits: + 14.40, grouping:'일반회원', selling: '0'},
+  {code: "290524", codename: '신도기연', price: "16,000원", purchase: '17,000원', profits: - 10.60, grouping:'관리자, 특별회원', selling: '0'},
+  {code: "093321", codename: '케이아이엔엑스', price: "44,000원", purchase: '72,000원', profits: - 38.60, grouping:'전체', selling: '0'},
+  {code: "102711", codename: '이엔에프테크놀로지', price: "29,000원", purchase: '44,000원', profits: - 34.60, grouping:'일반회원', selling: '0'},
+  {code: "192442", codename: '슈피겐코리아', price: "48,000원", purchase: '62,500원', profits: - 22.60, grouping:'특별회원', selling: '0'},
+  {code: "187873", codename: '디바이스이엔지', price: "30,400원", purchase: '38,100원', profits: - 20.20, grouping:'특별회원', selling: '0'},
+  {code: "294094", codename: '이오플로우', price: "55,800원", purchase: '55,100원', profits: + 1.5, grouping:'일반회원', selling: '0'},
+  {code: "200135", codename: '콜마비앤에이치', price: "35,800원", purchase: '49,100원', profits: - 29.10, grouping:'특별회원', selling: '0'},
+  {code: "353201", codename: '대덕전자', price: "17,050원", purchase: '18,200원', profits: - 6.30, grouping:'전체', selling: '0'},
+  {code: "030212", codename: 'KTB투자증권', price: "6,500원", purchase: '72,000원', profits: - 9.70, grouping:'관리자', selling: '0'},
+  {code: "003533", codename: '한화투자증권', price: "5,800원", purchase: '4,780원', profits: + 23.00, grouping:'전체', selling: '0'},
+  {code: "001454", codename: '현대해상', price: "28,800원", purchase: '25,200원', profits: + 11.30, grouping:'전체', selling: '0'},
+  {code: "066975", codename: '엘엔에프', price: "214,400원", purchase: '185,500원', profits: + 16.60, grouping:'일반회원', selling: '0'},
+  {code: "032646", codename: 'LG유플러스', price: "14,800원", purchase: '14,300원', profits: + 3.50, grouping:'특별회원', selling: '0'},
+  {code: "096247", codename: '청담러닝', price: "31,800원", purchase: '27,200원', profits: + 14.40, grouping:'일반회원', selling: '0'},
 
 
 ];
--- src/app/user_menu/dialog/user-completion-detail/user-completion-detail.component.ts
+++ src/app/user_menu/dialog/user-completion-detail/user-completion-detail.component.ts
@@ -1,16 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 import { MatBottomSheetRef } from '@angular/material/bottom-sheet';
+import {MatTableDataSource} from '@angular/material/table';
+
+export interface PeriodicElement {
+  Code: string;
+  CodeName: string;
+  Buying: string;
+  Selling: string;
+  Number: number;
+  Data: string;
+
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {Code:'86670',CodeName:'비앤티',Buying:'9,790',Selling:'10,100',Number: 3.20,Data:'3일',},
+  {Code:'267320',CodeName:'나인테크',Buying:'42,50',Selling:'4,460',Number: 4.90,Data:'11.23~24',},
+  {Code:'100130',CodeName:'동국S&C',Buying:'8,450',Selling:'8,661',Number: 2.50,Data:'11.23~24',},
+  {Code:'21080',CodeName:'에이티넘인베스트',Buying:'2,690',Selling:'3,012',Number: 12.00,Data:'11.23~24',},
+  {Code:'37350',CodeName:'성도이엔지',Buying:'4,100',Selling:'4,245',Number: 3.50,Data:'11.24~25',},
+  {Code:'86670',CodeName:'비앤티',Buying:'9,700',Selling:'10,700',Number: 9.30,Data:'11.20~25',},
+  {Code:'003850',CodeName:'보령제약',Buying:'15,000',Selling:'20,500',Number: 36.70,Data:'2달',},
+  {Code:'299030',CodeName:'하나기술',Buying:'86,500',Selling:'97,600',Number: 12.80,Data:'11.25~26',},
+  {Code:'006740',CodeName:'영풍제지',Buying:'4,615',Selling:'4,790',Number: 3.80,Data:'11.25~26',},
+  {Code:'100130',CodeName:'동국S&C',Buying:'8,260',Selling:'8,570',Number: 3.80,Data:'11.23~26',},
+  {Code:'86670',CodeName:'비앤티',Buying:'9,790',Selling:'10,100',Number: 3.20,Data:'3일',},
+  {Code:'267320',CodeName:'나인테크',Buying:'42,50',Selling:'4,460',Number: 4.90,Data:'11.23~24',},
+  {Code:'100130',CodeName:'동국S&C',Buying:'8,450',Selling:'8,661',Number: 2.50,Data:'11.23~24',},
+  {Code:'21080',CodeName:'에이티넘인베스트',Buying:'2,690',Selling:'3,012',Number: 12.00,Data:'11.23~24',},
+  {Code:'37350',CodeName:'성도이엔지',Buying:'4,100',Selling:'4,245',Number: 3.50,Data:'11.24~25',},
+  {Code:'86670',CodeName:'비앤티',Buying:'9,700',Selling:'10,700',Number: 9.30,Data:'11.20~25',},
+  {Code:'003850',CodeName:'보령제약',Buying:'15,000',Selling:'20,500',Number: 36.70,Data:'2달',},
+  {Code:'299030',CodeName:'하나기술',Buying:'86,500',Selling:'97,600',Number: 12.80,Data:'11.25~26',},
+  {Code:'006740',CodeName:'영풍제지',Buying:'4,615',Selling:'4,790',Number: 3.80,Data:'11.25~26',},
+  {Code:'100130',CodeName:'동국S&C',Buying:'8,260',Selling:'8,570',Number: 3.80,Data:'11.23~26',},
+
+];
 
 @Component({
   selector: 'app-user-completion-detail',
   templateUrl: './user-completion-detail.component.html',
   styleUrls: ['./user-completion-detail.component.css']
 })
+
 export class UserCompletionDetailComponent implements OnInit {
 
   constructor(private bottomSheetRef: MatBottomSheetRef<UserCompletionDetailComponent>) { }
 
+  displayedColumns: string[] = ['Code','CodeName', 'Buying', 'Selling', 'Number', 'Data'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+
+
+
+
   ngOnInit(): void {
+
   }
 
 	close() {
