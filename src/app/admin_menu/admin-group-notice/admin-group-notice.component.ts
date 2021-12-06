b21485dbf6bf60bcab2a230988b41a14c8dafaf0
김준형 <wnsgudcnt@gmail.com>
2021-12-06 17:53:13 +0900

    기능 업데이트 - 그룹별 알림

--- src/app/admin_menu/admin-group-notice/admin-group-notice.component.ts
+++ src/app/admin_menu/admin-group-notice/admin-group-notice.component.ts
@@ -12,10 +12,10 @@ import { AppState } from 'src/app/store';
 })
 export class AdminGroupNoticeComponent implements OnInit {
 
-  AllUserData:any =[];
-  AdminUserData:any =[];
-  SpecialUserData:any =[];
-  NomalUserData:any =[];
+  AllGroupUserData:any =[];
+
+
+
   constructor(
     private MatBottomSheet: MatBottomSheet,
     private firestore: Firestore,
@@ -23,22 +23,47 @@ export class AdminGroupNoticeComponent implements OnInit {
   ) { }
 
   async ngOnInit(): Promise<void> {
+    var GroupData:any =[];
+    await getDocs(collection(this.firestore, "groups")).then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        GroupData.push(doc.data());
+      });
+    })
     await getDocs(collection(this.firestore, "users")).then((querySnapshot)=>{
       var GroupUserData:any =[];
       querySnapshot.forEach((doc) => {
         GroupUserData.push(doc.data());
       });
 
-      GroupUserData.forEach(userdata => {
-        this.AllUserData.push(userdata)
-        if(userdata.group == '일반회원'){
-          this.NomalUserData.push(userdata)
-        }else if(userdata.group == '특별회원'){
-          this.SpecialUserData.push(userdata)
-        }else if(userdata.group == '관리자'){
-          this.AdminUserData.push(userdata)
-        }
+      GroupData.forEach(group => {
+        var groupArray:any=[]
+        GroupUserData.forEach(user => {
+          if (group.name === user.group) {
+            groupArray.push(user)
+          }
+        });
+        var groupData = {
+          name: group.name,
+          groupMember:groupArray
+        };
+        this.AllGroupUserData.push(groupData);
       });
+      var groupData = {
+        name: "전체",
+        groupMember:GroupUserData
+      };
+      this.AllGroupUserData.unshift(groupData)
+      // GroupUserData.forEach(userdata => {
+      //   this.AllUserData.push(userdata)
+      //   if(userdata.group == '일반회원'){
+      //     this.NomalUserData.push(userdata)
+      //   }else if(userdata.group == '특별회원'){
+      //     this.SpecialUserData.push(userdata)
+      //   }else if(userdata.group == '관리자'){
+      //     this.AdminUserData.push(userdata)
+      //   }
+      // });
+      console.log(this.AllGroupUserData);
 
     })
   }
